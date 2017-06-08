8d330be1fdd362e3920b20ed7c0a84daa33f048f
zenite <214114731@qq.com>
2017-06-08 17:51:47 +0800

    1

---
+++ javascripttest2/javascripttest/entity/NodeAttack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace javascripttest.entity
+{
+    public class NodeAttack : AbstractNode
+    {
+
+        public string x { get; set; }
+        public string y { get; set; }
+        public string chief { get; set; }
+        public string hand { get; set; }
+        public string city { get; set; }
+    }
+}
