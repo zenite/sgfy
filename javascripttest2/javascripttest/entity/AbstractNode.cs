403e391e0ad94423f7f3d7a90eab3b0a19c189cb
zenite <214114731@qq.com>
2017-06-08 17:51:25 +0800

    1

---
+++ javascripttest2/javascripttest/entity/AbstractNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace javascripttest.entity
+{
+    public class AbstractNode
+    {
+        public string name { get; set; }
+    }
+}
