21a02150c50b8750729a31d64d1c7575b81a7c38
zenite <214114731@qq.com>
2017-06-08 17:52:20 +0800

    111

---
+++ javascripttest2/javascripttest/BLL/HandlerAttack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using javascripttest.Regular;
+using System.Threading.Tasks;
+
+namespace javascripttest.BLL
+{
+    public class HandlerAttack
+    {
+        private string RootDirPath;
+        private string[] CreateXml ;
+        public HandlerAttack()
+        { 
+            
+        }
+
+    }
+}
